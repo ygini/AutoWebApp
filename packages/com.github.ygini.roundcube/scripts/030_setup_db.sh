693afb311ca39489578819b109afa3432e41a08e
Yoann Gini <yoann.gini@gmail.com>
2015-08-19 19:18:38 +0200

    Fix SQL path

--- packages/com.github.ygini.roundcube/scripts/030_setup_db.sh
+++ packages/com.github.ygini.roundcube/scripts/030_setup_db.sh
@@ -5,7 +5,7 @@ source "$scriptdir/shared"
 
 mkdir "$RC_DB_SQLITE_FOLDER"
 sqlite3 "$RC_DB_SQLITE_PATH" < "$RC_PATH/SQL/sqlite.initial.sql"
-sqlite3 "$RC_DB_SQLITE_PATH" < "$RC_PATH/plugins/carddav/dbinit/sqlite3"
+sqlite3 "$RC_DB_SQLITE_PATH" < "$RC_PATH/plugins/carddav/dbinit/sqlite3.sql"
 
 if [ $? -ne 0 ]; then
     exit 1
